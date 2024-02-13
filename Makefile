6be8b81a530bd51a1f005867d3b8d1e03c8ec92c
Nick Riley <nriley02@terpmail.umd.edu>
2024-02-12 21:21:36 -0500

    first bouncing_cube

--- Makefile
+++ Makefile
@@ -10,5 +10,8 @@ bin:
 bouncing_cube: bouncing_cube.cpp
 	$(CC) $(CFLAGS)  bouncing_cube.cpp -o bin/bouncing_cube $(SMFL_LIBS)
 
+display:
+	export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
+
 clean:
 	cd bin && rm -f *
---
+++ bouncing_cube.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+
+    const int CUBE_WIDTH = 50;
+    const int CUBE_HEIGHT = 50;
+
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Bouncing cube");
+
+    sf::RectangleShape cube(sf::Vector2f(CUBE_WIDTH, CUBE_HEIGHT));
+    cube.setFillColor(sf::Color::Red);
+
+
+
+    sf::Vector2f position(100, 100);
+    sf::Vector2f velocity(2, 2); 
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        position += velocity;
+
+        if (position.x < 0 || position.x + CUBE_WIDTH > WINDOW_WIDTH)
+            velocity.x = -velocity.x;
+        if (position.y < 0 || position.y + CUBE_HEIGHT > WINDOW_HEIGHT)
+            velocity.y = -velocity.y;
+
+        cube.setPosition(position);
+
+        window.clear();
+
+        window.draw(cube);
+
+        window.display();
+    }
+
+    return 0;
+}
